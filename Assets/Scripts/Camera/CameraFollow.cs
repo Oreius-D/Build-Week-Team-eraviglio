bd625d1875704034e494b370544387ba5779615c
AlessioGambale <alessiogambale24@gmail.com>
2025-12-09 23:51:15 +0100

    Creazione script CameraFollow

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
-{
+{      
+    //player da seguire
+    public Transform player;
+
+    //distanza tra camera e player
+    public Vector3 offset = new(0f, 0f, -10f);
+
+    //velocita` movimento camera
+    public float smoothSpeed = 0.5f;
+
+    //attivare i limiti della mappa e settarli
+    public bool enableBoundaries = true;
+    public float minX = 0f;
+    public float maxX = 0f;
+    public float minY = 0f;
+    public float maxY = 0f;
+    //chiedere a Paolo i limiti della mappa per settarli.
+   
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Ciao, sono un branch");
+        if (player == null)
+        {
+         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                player = playerObject.transform;
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        
+        if (enableBoundaries) 
+        {
+            smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minX, maxX);
+
+            smoothedPosition.y = Mathf.Clamp(smoothedPosition.y, minY, maxY);
+        }
+           transform.position = smoothedPosition;
     }
 }
