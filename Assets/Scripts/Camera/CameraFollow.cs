b81cbdd1717b2130930ea2349749caacb9ff4701
Francesco Rizzo <rizzofrancesco2508@gmail.com>
2025-12-09 19:39:38 +0100

    Aggiunta script per Camera
    Creato lo script per far sì che il player sia sempre followato

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ciao, sono un branch");
+    }
+}
