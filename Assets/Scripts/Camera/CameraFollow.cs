61ca4c8d14f5b0db8be6c77f8217451a7b95ddc0
AlessioGambale <alessiogambale24@gmail.com>
2025-12-11 19:44:38 +0100

    CameraFollow
    Mancano i limiti

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,61 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
-{      
-    //player da seguire
-    public Transform player;
+{
+    //Player da seguire
+    public Transform target;
+    //Distanza Camera-Target
+    public Vector3 offset;
+    //velocita` del movimento
+    [Range(0, 10)]
+    public float smoothness;
 
-    //distanza tra camera e player
-    public Vector3 offset = new(0f, 0f, -10f);
 
-    //velocita` movimento camera
-    public float smoothSpeed = 0.5f;
 
     //attivare i limiti della mappa e settarli
     //checkbox nell inspector
+
     public bool enableBoundaries = true;
     public float minX = 0f;
     public float maxX = 0f;
     public float minY = 0f;
     public float maxY = 0f;
     //chiedere a Paolo i limiti della mappa per settarli.
-   
-    // Start is called before the first frame update
-    //cerca il Player , gameobj con tag Player
-    void Start()
-    {
-        if (player == null)
-        {
-         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-            if (playerObject != null)
-            {
-                player = playerObject.transform;
-            }
-        }
-    }
 
     void LateUpdate()
     {
-        //se il player muore la cam fa zoomout
-        if (player == null)
-        { Vector3 zoomOutPosition = transform.position + new Vector3(0, 0, -5);
-         transform.position = Vector3.Lerp(transform.position , zoomOutPosition , Time.deltaTime);
+        Follow();
+    }
 
-         return;
-        }
-
-        Vector3 desiredPosition = player.position + offset;
-
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        
-        if (enableBoundaries) 
+    void Follow()
+    {
+        //posizione desiderata+smooth
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
+        //applica i limiti della mappa 
+        if (enableBoundaries)
         {
             smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minX, maxX);
-
             smoothedPosition.y = Mathf.Clamp(smoothedPosition.y, minY, maxY);
         }
-           transform.position = smoothedPosition;
+        transform.position = smoothedPosition;
+
     }
+
 }
